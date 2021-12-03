fbef228bfbe11750ca1bd12d34c8b624c19b29b8
Leis Al Ayoubi <leisayoubi@web.de>
2021-12-03 16:13:40 +0100

    Final version 1.0

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -54,4 +54,28 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  VectorXd z_pred = VectorXd(3);
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  //non-linear measurement function h(x)
+  z_pred << sqrt(pow(px,2) + pow(py,2)),
+            atan(py/px),
+            (px*vx + py*vy)/sqrt(pow(px,2) + pow(py,2));
+
+  VectorXd y = z - z_pred;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
